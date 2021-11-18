c3f4a091322192dc2be3288681afa691f542ad47
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2021-11-18 11:18:40 -0600

    how to grab content

--- grab-maincontent.sh
+++ grab-maincontent.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-HTML="$1"
-
-MAINHTML=${HTML/.html/.main.html}
-
-xmllint --xpath "//div[@id='mainContent']" "$HTML" > "$MAINHTML"
-
+IFS=$'\n'
+for PAGE in $(cat names.txt)
+do
+  RAWHTML="raw/$PAGE.html"
+  OUTHTML="clean/$PAGE.html"
+  OUTDIR=$(dirname $OUTHTML)
+  echo "Grabbing from $RAWHTML to ($OUTDIR) $OUTHTML"
+  if [ ! -d "$OUTDIR" ]
+  then
+    mkdir -p "$OUTDIR"
+  fi
+  cat <<HTMLHEAD >> "$OUTHTML"
+<html>
+<head>
+    <title>Jetty WTP Plugin</title>
+    <link rel="stylesheet" media="screen, print" href="wiki-style.css"/>
+</head>
+<body>
+HTMLHEAD
+  xmllint --xpath "//div[@id='mainContent']" "$RAWHTML" >> "$OUTHTML"
+  cat <<HTMLFOOT >> "$OUTHTML"
+</body>
+</html>
+HTMLFOOT
+done
